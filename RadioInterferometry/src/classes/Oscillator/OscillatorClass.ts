4c431b4913a8bef93e8207d616ae3dd22694f71f
Brodie Gilmour <brodie.gilmour@webappuk.com>
2015-01-23 16:48:54 +0000

    Completed Oscillator port, need to add typings and look into the applyEnvelope method.

--- RadioInterferometry/src/classes/Oscillator/OscillatorClass.ts
+++ RadioInterferometry/src/classes/Oscillator/OscillatorClass.ts
@@ -1,28 +1,24 @@
 ﻿export class Oscillator {
     private static waveTable: { [key: string]: Float32Array };
 
-    public static Sine = function (step) {
-        return Math.sin(2 * Math.PI * step);
+    public static Sine = function (step: number, phi?: number) {
+        return Math.sin((2 * Math.PI * step) + phi ? phi : 0);
     };
 
-    public static Cosine = function (step) {
-        return Math.cos(2 * Math.PI * step);
+    public static Cosine = function (step: number, phi?: number) {
+        return Math.cos((2 * Math.PI * step) + phi ? phi : 0);
     };
 
-    public static Square = function (step) {
+    public static Square = function (step: number, phi?: number) {
         return step < 0.5 ? 1 : -1;
     };
 
-    public static Saw = function (step) {
+    public static Saw = function (step: number, phi?: number) {
         return 2 * (step - Math.round(step));
     };
 
-    public static Triangle = function (step) {
+    public static Triangle = function (step: number, phi?: number) {
         return 1 - 4 * Math.abs(Math.round(step) - step);
-    };
-
-    public static Pulse = function (step) {
-        // stub
     };
 
     private _frequency: number;
@@ -105,5 +101,84 @@
         for (var i = 0; i < this._waveTableLength; i++) {
             Oscillator.waveTable[this._func][i] = this._func(i * waveTableHz / this._sampleRate);
         }
+    }
+
+    setAmp(amplitude) {
+        if (amplitude >= 0 && amplitude <= 1) {
+            this._amplitude = amplitude;
+        } else {
+            throw "Amplitude out of range (0..1).";
+        }
+    }
+  
+    /**
+     * Set the frequency of the signal
+     *
+     * @param {Number} frequency The frequency of the signal
+     */
+    setFreq(frequency) {
+        this._frequency = frequency;
+        this._cyclesPerSample = frequency / this._sampleRate;
+    }
+     
+    // Add an oscillator
+    add(oscillator) {
+        for (var i = 0; i < this._bufferSize; i++) {
+            //this.signal[i] += oscillator.valueAt(i);
+            this._signal[i] += oscillator.signal[i];
+        }
+
+        return this._signal;
+    }
+     
+    // Add a signal to the current generated osc signal
+    addSignal(signal) {
+        for (var i = 0; i < signal.length; i++) {
+            if (i >= this._bufferSize) {
+                break;
+            }
+            this._signal[i] += signal[i];
+   
+            /*
+            // Constrain amplitude
+            if ( this.signal[i] > 1 ) {
+              this.signal[i] = 1;
+            } else if ( this.signal[i] < -1 ) {
+              this.signal[i] = -1;
+            }
+            */
+        }
+        return this._signal;
+    }
+     
+    // Add an envelope to the oscillator
+    addEnvelope(envelope) {
+        this._envelope = envelope;
+    }
+
+    applyEnvelope() {
+        this._envelope.process(this._signal);
+    }
+
+    valueAt(offset) {
+        return this._waveTable[offset % this._waveTableLength];
+    }
+
+    generate() {
+        var frameOffset = this._frameCount * this._bufferSize;
+        var step = this._waveTableLength * this._frequency / this._sampleRate;
+        var offset;
+
+        for (var i = 0; i < this._bufferSize; i++) {
+            //var step = (frameOffset + i) * this.cyclesPerSample % 1;
+            //this.signal[i] = this.func(step) * this.amplitude;
+            //this.signal[i] = this.valueAt(Math.round((frameOffset + i) * step)) * this.amplitude;
+            offset = Math.round((frameOffset + i) * step);
+            this._signal[i] = this._waveTable[offset % this._waveTableLength] * this._amplitude;
+        }
+
+        this._frameCount++;
+
+        return this._signal;
     }
 }

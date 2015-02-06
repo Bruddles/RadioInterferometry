594360ccd69b38348cc7b63ebbc04c6d3c168b10
Brodie Gilmour <brodie.gilmour@webapplicationsuk.com>
2015-02-06 13:32:07 +0000

    Project restructure

--- RadioInterferometry/src/classes/Oscillator/OscillatorTypeEnum.js
+++
@@ -1,9 +0,0 @@
-var OscillatorType;
-(function (OscillatorType) {
-    OscillatorType[OscillatorType["Sine"] = 0] = "Sine";
-    OscillatorType[OscillatorType["Cosine"] = 1] = "Cosine";
-    OscillatorType[OscillatorType["Triangle"] = 2] = "Triangle";
-    OscillatorType[OscillatorType["Saw"] = 3] = "Saw";
-    OscillatorType[OscillatorType["Square"] = 4] = "Square";
-})(OscillatorType || (OscillatorType = {}));
-//# sourceMappingURL=OscillatorTypeEnum.js.map
